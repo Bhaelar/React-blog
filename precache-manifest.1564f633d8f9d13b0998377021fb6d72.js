d3882c2a785922a30e81a75b041fc6227c956084
Bhaelar <okenlabasit@gmail.com>
2020-07-17 00:33:39 +0100

    Updates

---
+++ precache-manifest.1564f633d8f9d13b0998377021fb6d72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924ddff28d84a9c9ece611b4ef4113a2",
+    "url": "/React-blog/index.html"
+  },
+  {
+    "revision": "bff908ac2fd67ecb2b1c",
+    "url": "/React-blog/static/js/2.1ade4174.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/React-blog/static/js/2.1ade4174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27dcf2ae8ede1948eacc",
+    "url": "/React-blog/static/js/main.ae18d776.chunk.js"
+  },
+  {
+    "revision": "6f985e4b1d381df6c245",
+    "url": "/React-blog/static/js/runtime-main.ec43955a.js"
+  }
+]);
